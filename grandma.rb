98097dece043aee38d85c4f3c6f11dc439ff6cc6
Siobhan The Destroyer <siobhancreagan@gmail.com>
2021-04-29 22:33:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(comment)
   if phrase.upcase != phrase
-puts "HUH?! SPEAK UP, SONNY!"
- elsif phrase == 'I LOVE YOU GRANDMA!'
- puts "I LOVE YOU TOO PUMPKIN!"
+puts HUH?! SPEAK UP, SONNY!
+ elsif phrase == I LOVE YOU GRANDMA!
+ puts I LOVE YOU TOO PUMPKIN!
  else
- puts "NO, NOT SINCE 1938!"
+ puts NO, NOT SINCE 1938!
 end
 end
